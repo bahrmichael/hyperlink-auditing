472d241f9cbd700f7e73f11c3c878d7feb8226fe
Michael Bahr <michael.bahr@live.de>
2021-04-23 18:05:46 +0200

    Don't sanitize path so that we can properly track outbound requests

--- src/functions/ping/handler.ts
+++ src/functions/ping/handler.ts
@@ -6,17 +6,13 @@ import {DynamoDB} from 'aws-sdk';
 const {TABLE_NAME} = process.env;
 const ddb = new DynamoDB.DocumentClient();
 
-function getPath(headers: any, key: 'ping-from' | 'ping-to'): string {
-    return headers[key].split(headers.Host)[1];
-}
-
 export const main = async (event) => {
 
     const item = {
         id: getId(event.requestContext),
         date: new Date().toISOString(),
-        from: getPath(event.headers, "ping-from"),
-        to: getPath(event.headers, "ping-to"),
+        from: event.headers["ping-from"],
+        to: event.headers["ping-to"],
     };
 
     await ddb.put({
